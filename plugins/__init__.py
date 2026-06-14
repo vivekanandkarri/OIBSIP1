a01d746c1e32790516fedd586ed36645481b7c4e
vivekanandkarri <karrivivek123@gmail.com>
2026-06-14 16:33:05 +0530

    Add plugins/__init__.py via PowerShell API upload

---
+++ plugins/__init__.py
@@ -0,0 +1,5 @@
+"""Plugins package for PyVoice Assistant.
+
+Any file placed in this directory containing a `register(assistant)` function
+will be automatically loaded by the orchestrator at startup.
+"""
